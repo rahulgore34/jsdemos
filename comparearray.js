b6fa4ec3917247643081327d77c2ae010f763310
rahulgore34 <rahulgore34@gmail.com>
2018-02-22 21:47:47 +0530

    two array compare

---
+++ comparearray.js
@@ -0,0 +1,24 @@
+var arr1 = [1,2,3];
+var arr2 = [1,2,4];
+
+function compareTwoArrays(arr1,arr2){
+     var areSame = false;
+	if(arr1.length !=arr2.length){
+		
+		areSame = false;
+	}else{
+		
+		for(var i=0;i<arr1.length;i++){
+			if(arr1[i] == arr2[i]){
+				areSame = true;
+			}else{
+				
+				areSame = false;
+			}
+		}
+	}
+
+	return areSame;
+}
+
+console.log(compareTwoArrays(arr1,arr2));
