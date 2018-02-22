47aae9c0168ab454ee6a6298359c00675805040c
rahulgore34 <rahulgore34@gmail.com>
2018-02-22 21:30:51 +0530

    updated to add index of obj in array

--- indexofObj.js
+++ indexofObj.js
@@ -7,3 +7,19 @@ console.log(arr1.indexOf(3)); //first index 2
 console.log(arr1.indexOf(3,3)); //first index 7 after 3rd index 
 //first parameter as value for which need to find index
 //second paramter as index from where need to find index
+
+////////
+//find index of obj in array
+
+var arr = [
+
+ {name:"rahul"},
+ {name:"sarita"},
+ {name:"shreya"}
+];
+
+var i = arr.map(function(item){
+ return item.name; //returns new array with name ["rahul"...]
+}).indexOf("shreya"); //now find index of name
+
+ console.log(i);
